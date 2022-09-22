fa4b11e859fcb2385a1f3171f0890a1726f04e73
Leonid Haidash <Gaydashleo@gmail.com>
2022-09-22 19:10:09 +0300

    search

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,41 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { Container } from './App.styled';
+import{SearchBar} from 'components/Searchbar/Searchbar'
+
+export class App extends Component {
+  state = {
+    images:[],
+    query: '',
+    page: 1,
+    error: null,
+  }
+
+  handleSearch = query => {
+    if (query === this.state.query) return;
+    this.setState({
+      images:[],
+      query,
+      page: 1,
+      error: null,
+    })
+  }
+
+  render() {
+
+     return (
+       <Container>
+         <SearchBar onSubmit={this.handleSearch}/>
+
+      <ToastContainer
+position="bottom-center"
+autoClose={5000}
+closeOnClick
+/>
+   </Container>
   );
 };
+  }
+ 
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,1 +1,66 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+import {
+ Header,
+  Form,
+  Input,
+  Button,
+} from './SearchBar.styled';
+
+
+
+
+export class SearchBar extends Component{
+  static propTypes = {
+    onSubmit: PropTypes.func,
+  };
+
+  state = {
+    query:'',
+  };
+
+  handleChange = event => {
+    this.setState({ query: event.Target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.state.query.trim() === '') {
+      toast.warn('Please specify your query!');
+      return;
+    }
+    this.props.onSubmit(this.state.query);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ query: ''});
+  }
+
+  render = () => {
+    const { query } = this.state;
+  
+    return (
+ <Header>
+  <Form >
+    <Button type="submit" >
+      <span class="button-label">Search</span>
+    </Button>
+
+    <Input
+      type="text"
+      name="query"
+      value={query}
+      onChange={this.handleChange}
+      autocomplete="off"
+      autofocus
+      placeholder="Search images and photos"
+    />
+  </Form>
+</Header>
+    )
+  }
+
+
+}
