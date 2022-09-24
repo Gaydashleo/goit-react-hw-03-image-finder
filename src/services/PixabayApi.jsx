1718dfa5461008ee860f04c568638c40da5c21d9
Leonid Haidash <Gaydashleo@gmail.com>
2022-09-24 16:08:00 +0300

    Update PixabayApi.jsx

--- src/services/PixabayApi.jsx
+++ src/services/PixabayApi.jsx
@@ -5,6 +5,6 @@ const BASE_URL = 'https://pixabay.com';
 
 export const fetchData = (query, page, perPage) => {
   return axios
-    .get(`${BASE_URL}/api/?q=${query}&page={page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`)
+    .get(`${BASE_URL}/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`)
     .then(response => response.data);
 };
